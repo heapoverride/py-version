c21834c9474c9d525fcb82159b29386d5622a5aa
UnrealSecurity <arran.bishop89@aol.com>
2022-08-22 01:10:14 +0300

    Updated main

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ def main():
     a = Version("2.3")
     b = Version(major=2, minor=2)
 
-    print(repr(a), repr(b))
-
+    print("COMPARISON OPERATIONS")
     print(a, "==", b, "=", a == b)
     print(a, "!=", b, "=", a != b)
     print(a,  "<", b, "=", a  < b)
     print(a,  ">", b, "=", a  > b)
     print(a, ">=", b, "=", a >= b)
     print(a, "<=", b, "=", a <= b)
+
+    print("BINARY OPERATIONS")
+    print(a + b)
+    print(((a + b) - a))
 
 if __name__ == "__main__":
     main()
