a1ed8775232d6e4d0b53e18b527bdf69f0a104e9
UnrealSecurity <arran.bishop89@ol.com>
2022-08-22 00:37:34 +0300

    Initial commit

---
+++ version/version.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+import re
+
+class Version:
+    def __init__(self, version: str = None, *, 
+        major: int = 0, 
+        minor: int = 0, 
+        build: int = 0, 
+        revision: int = 0
+    ) -> None:
+        """Version 
+
+        Args:
+            version (str, optional): Parse version from string.
+            major (int, optional): Major version number. Defaults to 0.
+            minor (int, optional): Minor version number. Defaults to 0.
+            build (int, optional): Build number. Defaults to 0.
+            revision (int, optional): Revision number. Defaults to 0.
+
+        Hint:
+            Version("1.8")
+            Version(major=1, minor=8)
+        """
+        self.__major = major
+        self.__minor = minor
+        self.__build = build
+        self.__revision = revision
+        
+        if type(version) == str:
+            try:
+                values = [int(x) for x in re.split(r"[\.-]", version, maxsplit=4)]
+                length = len(values)
+
+                if length > 0: 
+                    self.__major = values[0]
+
+                    if length > 1: 
+                        self.__minor = values[1]
+
+                        if length > 2: 
+                            self.__build = values[2]
+
+                            if length > 3: 
+                                self.__revision = values[3]
+            except:
+                raise Exception(f"Invalid version: {repr(version)}")
+
+    @property
+    def major(self) -> int:
+        """Major version number"""
+        return self.__major
+
+    @major.setter
+    def major(self, value: int) -> int:
+        self.__major = value
+
+    @property
+    def minor(self) -> int:
+        """Minor version number"""
+        return self.__minor
+        
+    @minor.setter
+    def minor(self, value: int) -> int:
+        self.__minor = value
+
+    @property
+    def build(self) -> int:
+        """Build number"""
+        return self.__build
+        
+    @build.setter
+    def build(self, value: int) -> int:
+        self.__build = value
+
+    @property
+    def revision(self) -> int:
+        """Revision number"""
+        return self.__revision
+        
+    @revision.setter
+    def revision(self, value: int) -> int:
+        self.__revision = value
+
+    def __str__(self) -> str:
+        if self.build + self.revision > 0:
+            return f"{self.major}.{self.minor}.{self.build}.{self.revision}"
+        return f"{self.major}.{self.minor}"
+
+    def __repr__(self) -> str:
+        return f"Version({repr(str(self))})"
+
+    def __eq__(self, other: Version) -> bool:
+        if type(other) != Version: return False
+        return self.major == other.major \
+            and self.minor == other.minor \
+            and self.build == other.build \
+            and self.revision == other.revision
+
+    def __ne__(self, other: Version) -> bool:
+        return not self.__eq__(other)
+
+    def __gt__(self, other: Version) -> bool:
+        if type(other) != Version: return False
+        return self.major > other.major \
+            or self.minor > other.minor \
+            or self.build > other.build \
+            or self.revision > other.revision
+
+    def __lt__(self, other: Version) -> bool:
+        if type(other) != Version: return False
+        return self.major < other.major \
+            or self.minor < other.minor \
+            or self.build < other.build \
+            or self.revision < other.revision
+
+    def __ge__(self, other: Version):
+        return self.__eq__(other) or self.__gt__(other)
+
+    def __le__(self, other: Version):
+        return self.__eq__(other) or self.__lt__(other)
